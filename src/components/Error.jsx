efe9b02bb3b7059bfb17a2bae71b4ebd153b31a3
Nam <doanngocnam3002@gmail.com>
2023-07-20 23:58:22 +0700

    discoverpage

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-
-const Error = () => (
-  <div>Error</div>
-);
+const Error = () => <div>Error</div>;
 
 export default Error;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,22 @@
-const Discover = () => <div>Discover</div>;
+import Error from "../components/Error";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  console.log(genres);
+  return (
+    <div className="flex flex-col w-full    ">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold caret-lime-500 p-4 text-3xl text-green-500 text-left">
+          Discover the vibe
+        </h2>
+        <select onChange={()={}} value="" className="bg-slate-800 text-slate-50 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
+            {genres.map((gen)=>{<option key={gen.value}>
+                {gen.title}
+            </option>})}
+        </select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
